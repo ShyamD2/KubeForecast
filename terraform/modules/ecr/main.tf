3026f80a7e5bd323769cc6c23cc283cf057e7871
Shyam Kumar <dshyamkumar021@gmail.com>
2026-09-06 22:21:01 +0530

    chore(infra): enable force_delete on ECR repositories for teardown reliability

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -21,6 +21,7 @@ resource "aws_ecr_repository" "repos" {
   for_each             = toset(local.repositories)
   name                 = "${var.environment}-${each.key}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
